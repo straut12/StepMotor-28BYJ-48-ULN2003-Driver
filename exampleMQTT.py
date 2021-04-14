a09a167284779004763e14d8a5f8fd4fdd360fbb
sj4u <stemjust4u@gmail.com>
2021-04-14 02:59:11 -0600

    added dashboard

--- exampleMQTT.py
+++ exampleMQTT.py
@@ -32,32 +32,36 @@ if __name__ == "__main__":
         if rc==0:
             mqtt_client.connected = True          # If rc = 0 then successful connection
             client.subscribe(MQTT_SUB_TOPIC1)      # Subscribe to topic
+            client.subscribe(MQTT_SUB_TOPIC2)
             logging.info("Successful Connection: {0}".format(str(rc)))
             logging.info("Subscribed to: {0}\n".format(MQTT_SUB_TOPIC1))
+            logging.info("Subscribed to: {0}\n".format(MQTT_SUB_TOPIC2))
         else:
             mqtt_client.failed_connection = True  # If rc != 0 then failed to connect. Set flag to stop mqtt loop
             logging.info("Unsuccessful Connection - Code {0}".format(str(rc)))
 
     def on_message(client, userdata, msg):
         """on message callback will receive messages from the server/broker. Must be subscribed to the topic in on_connect"""
-        global newmsg, incomingD
+        global newmsg, incomingD, interval
+        if msg.topic == MQTT_SUB_TOPIC2:
+            interval = json.loads(str(msg.payload.decode("utf-8", "ignore")))
         if msg.topic == MQTT_SUB_TOPIC1:
             incomingD = json.loads(str(msg.payload.decode("utf-8", "ignore")))  # decode the json msg and convert to python dictionary
-            newmsg = True
+            #newmsg = True
             # Debugging. Will print the JSON incoming payload and unpack the converted dictionary
-            logging.debug("Receive: msg on subscribed topic: {0} with payload: {1}".format(msg.topic, str(msg.payload))) 
-            logging.debug("Incoming msg converted (JSON->Dictionary) and unpacking")
-            for key, value in incomingD.items():
-                logging.debug("{0}:{1}".format(key, value))
+            #logging.debug("Receive: msg on subscribed topic: {0} with payload: {1}".format(msg.topic, str(msg.payload))) 
+            #logging.debug("Incoming msg converted (JSON->Dictionary) and unpacking")
+            #for key, value in incomingD.items():
+            #    logging.debug("{0}:{1}".format(key, value))
 
     def on_publish(client, userdata, mid):
         """on publish will send data to broker"""
         #Debugging. Will unpack the dictionary and then the converted JSON payload
-        logging.debug("msg ID: " + str(mid)) 
-        logging.debug("Publish: Unpack outgoing dictionary (Will convert dictionary->JSON)")
-        for key, value in outgoingD.items():
-            logging.debug("{0}:{1}".format(key, value))
-        logging.debug("Converted msg published on topic: {0} with JSON payload: {1}\n".format(MQTT_PUB_TOPIC1, json.dumps(outgoingD))) # Uncomment for debugging. Will print the JSON incoming msg
+        #logging.debug("msg ID: " + str(mid)) 
+        #logging.debug("Publish: Unpack outgoing dictionary (Will convert dictionary->JSON)")
+        #for key, value in outgoingD.items():
+        #    logging.debug("{0}:{1}".format(key, value))
+        #logging.debug("Converted msg published on topic: {0} with JSON payload: {1}\n".format(MQTT_PUB_TOPIC1, json.dumps(outgoingD))) # Uncomment for debugging. Will print the JSON incoming msg
         pass 
 
     def on_disconnect(client, userdata,rc=0):
@@ -87,15 +91,25 @@ if __name__ == "__main__":
     @dataclass
     class Machine:
         stepper: List[StepperMotor]
-        delay: float
+        delay: float    # Future use as separate delay per motor
 
     m1 = StepperMotor([14, 15, 18, 23], 0, 0, [0,0,0,0,0], {"Harr1":[0,1], "Farr1":[0,1], "arr2":[0,1], "HarrOUT":[0,1], "FarrOUT":[0,1]})
     m2 = StepperMotor([19, 13, 6, 5], 0, 0, [0,0,0,0,0], {"Harr1":[0,1], "Farr1":[0,1], "arr2":[0,1], "HarrOUT":[0,1], "FarrOUT":[0,1]})
-
-    mach = Machine([m1, m2], 0.0015)
-
+    
     GPIO.setmode(GPIO.BCM)
+    FULLREVOLUTION = 4064    # Steps per revolution
+    DEFAULTDELAY = 1.6       # default delay in msec
+    startstepping, targetstep, debug1 = [],[],[]
+    mach = Machine([m1, m2], DEFAULTDELAY)
     for i in range(len(mach.stepper)):          # Setup each stepper motor
+        mach.stepper[i].speed[2] = [0,0,0,0]
+        startstepping.append(False)  # Flag for increment stepping function
+        targetstep.append(0)         # Flag for increment stepping function
+        debug1.append(True)           # Flag for debugging loop
+        for rotation in range(2):        # Setup each pin in each stepper
+            mach.stepper[i].coils["Harr1"][rotation] = [0,1,1,0]
+            mach.stepper[i].coils["Farr1"][rotation] = [0,1,1,0]
+            mach.stepper[i].coils["arr2"][rotation] = [0,1,1,0]
         for pin in mach.stepper[i].pins:        # Setup each pin in each stepper
             GPIO.setup(pin,GPIO.OUT)
             logging.info("pin {0} Setup".format(pin))
@@ -107,6 +121,7 @@ if __name__ == "__main__":
     MQTT_PASSWORD = user_info[1]                  # Replace with your mqtt password
     MQTT_CLIENT_ID = 'pi4'
     MQTT_SUB_TOPIC1 = 'pi/stepper'
+    MQTT_SUB_TOPIC2 = 'pi/stepper/interval'
     MQTT_PUB_TOPIC1 = 'pi/stepper/status'
 
     #==== START/BIND MQTT FUNCTIONS ====#
@@ -134,8 +149,21 @@ if __name__ == "__main__":
     #==== MAIN LOOP ====================#
     # MQTT setup is successful. Initialize dictionaries and start the main loop.
     
+    def stepupdate(spd, stp):
+        if spd == 3:
+            stp += 1
+        elif spd ==4:
+            stp += 2
+        elif spd == 1:
+            stp -= 1
+        elif spd == 0:
+            stp -= 2
+        else:
+            stp = stp
+        return stp
+
     def motors(command):
-        global mach
+        global mach, outgoingD, startstepping, targetstep, debug1, i
 
         for i in range(len(mach.stepper)):          # Setup each stepper motor
             for rotation in range(2):        # Setup each pin in each stepper
@@ -155,44 +183,44 @@ if __name__ == "__main__":
             mach.stepper[i].speed[1] = mach.stepper[i].coils["HarrOUT"][0]
             mach.stepper[i].speed[3] = mach.stepper[i].coils["HarrOUT"][1]
             mach.stepper[i].speed[4] = mach.stepper[i].coils["FarrOUT"][1]
-            if command["restart"][i] == 1:
-                mach.stepper[i].mode = 0
-                command["restart"][i] = 0
-                logging.debug("Motor:{0} Steps:{1} Mode:{2} Rot:{3} coils:{4}".format(i, mach.stepper[i].step, mach.stepper[i].mode, command["speed"][i], mach.stepper[i].speed[command["speed"][i]]))
-            if command["speed"][i] == 2 or mach.stepper[i].step > 6000 or mach.stepper[i].mode == 1: # motor stopped or completed a revolution
-                #logging.debug("Motor:{0} Steps:{1} Mode:{2} Rot:{3} coils:{4}".format(i, mach.stepper[i].step, command["mode"][i], command["speed"][i], mach.stepper[i].speed[command["speed"][i]]))
+            stepspeed = command["speed"][i]         # stepspeed is a temporary variable for this loop
+            if command["mode"][i] == 1 and command["startstep"][i] == 1:
+                startstepping[i] = True
+                logging.debug("2:STRTSTP ON - Motor:{0} Mode:{1} startstep:{2} startstepping:{3} machStep:{4} targetstep:{5}".format(i, command["mode"][i], command["startstep"][i], startstepping[i], mach.stepper[i].step, targetstep[i]))
+                debug1[i] = True
+            if command["mode"][i] == 1 and not startstepping[i]:
+                stepspeed = 2
+                if abs(mach.stepper[i].step) + command["step"][i] <= FULLREVOLUTION:
+                    targetstep[i] = abs(mach.stepper[i].step) + command["step"][i]
+                else:
+                    targetstep[i] = FULLREVOLUTION
+                if debug1[i]:
+                    logging.debug("1:MODE1      - Motor:{0} Mode:{1} startstep:{2} startstepping:{3} machStep:{4} targetstep:{5}".format(i, command["mode"][i], command["startstep"][i], startstepping[i], mach.stepper[i].step, targetstep[i]))
+                    debug1[i] = False
+            elif command["mode"][i] == 1 and startstepping[i]:
+                logging.debug("3:STEPPING   - Motor:{0} Mode:{1} startstep:{2} startstepping:{3} machStep:{4} targetstep:{5}".format(i, command["mode"][i], command["startstep"][i], startstepping[i], mach.stepper[i].step, targetstep[i]))
+                if abs(mach.stepper[i].step) >= targetstep[i]:
+                    logging.debug("4:DONE-M1OFF - Motor:{0} Mode:{1} startstep:{2} startstepping:{3} machStep:{4} targetstep:{5}".format(i, command["mode"][i], command["startstep"][i], startstepping[i], mach.stepper[i].step, targetstep[i]))
+                    startstepping[i] = False
+                    command["startstep"][i] = 0
+            GPIO.output(mach.stepper[i].pins, mach.stepper[i].speed[stepspeed])
+            mach.stepper[i].step = stepupdate(stepspeed, mach.stepper[i].step)
+            if stepspeed != 2 and (abs(mach.stepper[i].step) % interval[i]) == 0 :
+                outgoingD['motori'] = i
+                outgoingD['stepsi'] = mach.stepper[i].step
+                mqtt_client.publish(MQTT_PUB_TOPIC1, json.dumps(outgoingD))
+            if (abs(mach.stepper[i].step) > FULLREVOLUTION):  # If want to step past full revolution then need to add 'not startstepping'
+                logging.debug("FULL REVOLUTION -- Motor:{0} Steps:{1} Mode:{2} startstepping:{3} coils:{4}".format(i, mach.stepper[i].step, command["mode"][i], startstepping[i], mach.stepper[i].speed[command["speed"][i]]))
                 mach.stepper[i].step = 0
-            else:
-                mach.stepper[i].step += 1
-            if (command["mode"][i] == 1 and mach.stepper[i].step >= command["step"][i]) or mach.stepper[i].mode == 1:    # Stepping mode
-                #logging.debug("Motor:{0} Steps:{1} Mode:{2} Rot:{3} coils:{4}".format(i, mach.stepper[i].step, command["mode"][i], command["speed"][i], mach.stepper[i].speed[command["speed"][i]]))
-                GPIO.output(mach.stepper[i].pins, mach.stepper[i].speed[2])               # Hit step limit, stop
-                mach.stepper[i].mode = 1
-            else:
-                GPIO.output(mach.stepper[i].pins, mach.stepper[i].speed[command["speed"][i]])
-            #logging.debug("Motor:{0} Steps:{1} Mode:{2} Rot:{3} coils:{4}".format(i, mach.stepper[i].step, command["mode"][i], command["speed"][i], mach.stepper[i].speed[command["speed"][i]]))
         sleep(float(command["delay"])/1000)
-
-    for i in range(len(mach.stepper)):          # Setup each stepper motor
-        mach.stepper[i].speed[2] = [0,0,0,0]
-        for rotation in range(2):        # Setup each pin in each stepper
-            mach.stepper[i].coils["Harr1"][rotation] = [0,1,1,0]
-            mach.stepper[i].coils["Farr1"][rotation] = [0,1,1,0]
-            mach.stepper[i].coils["arr2"][rotation] = [0,1,1,0]
-
+    
+    interval = [254, 254]
     outgoingD = {}
-    incomingD = {"delay":1.5, "speed":[3,3], "mode":[0,0], "step":[4064, 4064], "restart":[0,0]}
-    newmsg = False
+    incomingD = {"delay":DEFAULTDELAY, "speed":[2,2], "mode":[0,0], "step":[FULLREVOLUTION, FULLREVOLUTION], "startstep":[0,0]}
+    #newmsg = False
     try:
         while True:
             motors(incomingD)
-            if newmsg:                                 # INCOMING: New msg/instructions have been received
-                for key, value in incomingD.items():
-                    motor_id = key
-                    speed_status = str(value)
-                    outgoingD[motor_id + 'i'] = speed_status      
-                mqtt_client.publish(MQTT_PUB_TOPIC1, json.dumps(outgoingD))
-                newmsg = False                         # Reset the new msg flag
     except KeyboardInterrupt:
         logging.info("Pressed ctrl-C")
     finally:
